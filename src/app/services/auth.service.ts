fa3ed74a8a31116749d3a8ce3e108eb9be2f5209
Kaan Cem Günaydın <gunaydnk@gmail.com>
2026-06-20 22:06:05 +0300

    fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = `${environment.apiUrl}/applications`;
+  private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) {}
 
